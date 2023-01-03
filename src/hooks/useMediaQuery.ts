42aed3546913e6e0eb5b732f50f2f8dc6aada8a6
@babinm1h <babinmih@mail.ru>
2023-01-03 08:18:49 +0300

    useMediaQuery fix

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,7 +1,15 @@
 import React from "react";
 
 export const useMediaQuery = (query: string) => {
-  const [matches, setMatches] = React.useState(false);
+  const getMatches = (query: string): boolean => {
+    // Prevents SSR issues
+    if (typeof window !== "undefined") {
+      return window.matchMedia(query).matches;
+    }
+    return false;
+  };
+
+  const [matches, setMatches] = React.useState(getMatches(query));
 
   function handleChange(e: MediaQueryListEvent) {
     setMatches(e.matches);
@@ -9,6 +17,7 @@ export const useMediaQuery = (query: string) => {
 
   React.useEffect(() => {
     const matchQueryList = window.matchMedia(query);
+    console.log(matchQueryList);
 
     matchQueryList.addEventListener("change", handleChange);
 
